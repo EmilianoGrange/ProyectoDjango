a2abbaed6b86c76176f3539a8773fba9f185b839
EmilianoGrange <milevCo@outlook.com>
2024-05-29 19:48:36 -0300

    Parciales de los templates

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from ecommerce.views import home_template, create_product, show_products, show_employees, show_suppliers
 
 urlpatterns = [
-    path('', home_template, name="home"),
+    path('', home_template, name='home'),
     path('add-product/', create_product),
-    path('products/', show_products, name="products"),
-    path('employees/', show_employees, name="employees"),
-    path('suppliers/', show_suppliers, name="suppliers"),
+    path('products/', show_products, name='products'),
+    path('employees/', show_employees, name='employees'),
+    path('suppliers/', show_suppliers, name='suppliers'),
 ]
